725fa5f1f9aebbc3be04583b0c6bceb5a770f1b0
Charles Bryce Steele <stainlesschuck@gmail.com>
2016-11-16 13:16:13 -0600

    Ran db:migrate after adding pages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113230037) do
+ActiveRecord::Schema.define(version: 20161116191037) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -88,6 +88,13 @@ ActiveRecord::Schema.define(version: 20161113230037) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
