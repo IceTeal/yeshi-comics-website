00e14aaf1f0c491eb5ac8d0d11b448849733c0f3
Charles Bryce Steele <stainlesschuck@gmail.com>
2016-12-03 17:09:29 -0600

    Fixed missing comma in controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,18 +1,18 @@
 class ChargesController < ApplicationController
 	def new
-		@amount = 500
+		@amount = 500 #change to shopping cart
 		@description = 'Description of Charge'
 	end
 
 	def create
-		amount = 500
+		amount = 500 #change to shopping cart
 
 		@customer = Stripe::Customer.create(email: params[:stripeEmail],
 											source: params[:stripeToken])
 
 		@charge = Stripe::Customer.create(customer: 	 @customer.id,
 											amount: 	 amount,
-											description: 'Rails Stripe customer'
+											description: 'Rails Stripe customer',
 											currency: 	 'cad')
 
 	rescue Stripe::CardError => e
