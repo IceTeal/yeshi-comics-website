682086e1fa591b8e3474696b799bf7ed4b1244e9
Charles Bryce Steele <stainlesschuck@gmail.com>
2016-12-04 16:21:59 -0600

    Retrieve all relevant data from Strip customer and add to database

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,44 @@
 class ChargesController < ApplicationController
 	def new
-		@amount = current_order.total #change to shopping cart
-		@description = 'Description of Charge'
+		@amount = current_order.total.to_i #change to shopping cart
+		@description = 'Your Purchase'
 	end
 
 	def create
 
+		@order = current_order
 
-		@amount = current_order.total #change to shopping cart
+		@amount = @order.total.to_i #change to shopping cart
 
-		@customer = Stripe::Customer.create(email: params[:stripeEmail],
+		@stripe_customer = Stripe::Customer.create(email: params[:stripeEmail],
 											source: params[:stripeToken])
 
-		@charge = Stripe::Customer.create(customer: 	 @customer.id,
-											amount: 	 @amount,
-											description: 'Rails Stripe customer',
-											currency: 	 'cad')
+        @charge = Stripe::Charge.create(customer:    @stripe_customer.id,
+	                                    amount:      @amount,
+	                                    description: 'Rails Stripe customer',
+	                                    currency:    'cad')
 
-		current_order.payment_type = "Stripe"
+        @customer = Customer.new
+
+
+		@response_data = @stripe_customer.sources.first
+
+
+        @customer.first_name = @response_data.name
+        @customer.email = @stripe_customer.email
+        @customer.address = "#{@response_data.address_line1}, #{@response_data.address_city}, #{@response_data.address_state}"
+
+        @customer.save
+
+
+		@order.payment_type = "Stripe"
+		@order.status = 2
+		@order.date = Date.current
+		@order.customer = @customer
+		@order.save	
+
+		session[:order_id] = nil
+
 	rescue Stripe::CardError => e
 		flash[:error] = e.message
 		redirect_to new_charge_path
