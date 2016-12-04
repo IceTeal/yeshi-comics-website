8bb3c7b3eecca9a7257292813f8f416d41ba9c93
Charles Bryce Steele <stainlesschuck@gmail.com>
2016-12-03 21:24:42 -0600

    Changed charges controller to pull total from current_order

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,20 +1,23 @@
 class ChargesController < ApplicationController
 	def new
-		@amount = 500 #change to shopping cart
+		@amount = current_order.total #change to shopping cart
 		@description = 'Description of Charge'
 	end
 
 	def create
-		amount = 500 #change to shopping cart
+
+
+		@amount = current_order.total #change to shopping cart
 
 		@customer = Stripe::Customer.create(email: params[:stripeEmail],
 											source: params[:stripeToken])
 
 		@charge = Stripe::Customer.create(customer: 	 @customer.id,
-											amount: 	 amount,
+											amount: 	 @amount,
 											description: 'Rails Stripe customer',
 											currency: 	 'cad')
 
+		current_order.payment_type = "Stripe"
 	rescue Stripe::CardError => e
 		flash[:error] = e.message
 		redirect_to new_charge_path
